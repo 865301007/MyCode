2efdebb93f201e58422a3b3369a9ec1351894cf1
Wuchenwcf <280298985@qq.com>
2018-09-02 23:48:27 +0800

    删除代码

--- JAVA/汤凯的代码/OrderList/src/main/java/indi/tangkai/orderlist/service/OrderGoodsService.java
+++
@@ -1,9 +0,0 @@
-package indi.tangkai.orderlist.service;
-
-import java.util.List;
-
-import indi.tangkai.orderlist.data.entity.OrderGoods;
-
-public interface OrderGoodsService {
-	public List<OrderGoods> getAllOrdersByOrderId(long order_id);
-}
--- JAVA/汤凯的代码/OrderList/src/main/java/indi/tangkai/orderlist/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package indi.tangkai.orderlist.service;
-
-import indi.tangkai.orderlist.data.entity.User;
-
-public interface UserService {
-	public User getUserByUserId(long user_id);
-}
